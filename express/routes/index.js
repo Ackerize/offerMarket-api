0a990c19379efed5ac461f76571b33cf2bebbdac
David Vallecios <00078217@uca.edu.sv>
2021-10-17 13:45:46 -0600

    modificando webpack config

--- express/routes/index.js
+++ express/routes/index.js
@@ -38,7 +38,7 @@ const swaggerDocs = swaggerJsDoc(swaggerOptions);
 
 router.use(
   "/api/v1/docs",
-  express.static("node_modules/swagger-ui-dist/", { index: false }),
+  express.static('node_modules/swagger-ui-dist/', { index: false }),
   swaggerUi.serve,
   swaggerUi.setup(swaggerDocs)
 );
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,17 @@
 const CopyPlugin = require("copy-webpack-plugin");
+const path = require("path");
 
 module.exports = {
   plugins: [
     new CopyPlugin([
       {
-        from: path.resolve(__dirname, "node_modules/swagger-ui-dist/"),
-        to: "node_modules/swagger-ui-dist",
+        from: path.resolve(__dirname, 'node_modules/swagger-ui-dist/'),
+        to: 'node_modules/swagger-ui-dist',
         test: /\.(js|css|html|png)$/i,
-        ignore: ["index.js", "absolute-path.js", "*.map"],
+        ignore: ['index.js', 'absolute-path.js', '*.map'],
       },
     ]),
   ],
+  mode: 'production',
+  target: 'node',
 };
