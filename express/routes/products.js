6b379de9cf4361661c1127139fb4d578cb28659c
David Vallecios <00078217@uca.edu.sv>
2021-10-17 23:51:42 -0600

    Agregando dos nuevas rutas

--- express/routes/products.js
+++ express/routes/products.js
@@ -8,11 +8,16 @@ const productController = require('../controllers/productController');
  * @swagger
  * /products:
  *  get:
- *      summary: Obtener todos los productos
+ *      summary: Obtener todos los productos en base a filtros
  *      description: Obtiene todos los productos
  *      tags: 
  *          - Productos
  *      parameters:
+ *          - in: query
+ *            name: category
+ *            schema:
+ *              type: string
+ *            description: Categoría del producto
  *          - in: query
  *            name: sort
  *            schema:
@@ -121,6 +126,134 @@ const productController = require('../controllers/productController');
  *                                  example: Error en el servidor
  */
 router.get('/', productController.getAll);
+
+/**
+ * @swagger
+ * /products/user/{uid}:
+ *  get:
+ *      summary: Obtener todos los productos de un usuario
+ *      description: Obtiene todos los productos de un usuario
+ *      tags: 
+ *          - Productos
+ *      parameters:
+ *          - in: path
+ *            name: uid
+ *            schema:
+ *              type: string
+ *            description: uid del usuario
+ *          - in: query
+ *            name: category
+ *            schema:
+ *              type: string
+ *            description: Categoría del producto
+ *          - in: query
+ *            name: sort
+ *            schema:
+ *              type: string
+ *              enum: [asc, desc]
+ *            description: Tipo de ordenamiento (ascendente o descendente)
+ *          - in: query
+ *            name: sortby
+ *            schema:
+ *              type: string
+ *              enum: [name, price, createdAt]
+ *            description: Propiedad en la cual se basará el ordenamiento
+ *          - in: query
+ *            name: page
+ *            schema:
+ *              type: string
+ *            description: Número de página
+ *          - in: query
+ *            name: size
+ *            schema:
+ *              type: string
+ *            description: Productos a mostrar por página
+ *      responses:
+ *          '200':
+ *              description: Productos obtenidos exitosamente
+ *              content:
+ *                  application/json:
+ *                     schema:
+ *                        type: object
+ *                        properties:
+ *                           error:
+ *                              type: boolean
+ *                              description: Indica que no hubo error
+ *                              example: false
+ *                           products:
+ *                              type: array
+ *                              description: Arreglo de productos
+ *                              items:
+ *                                  type: object
+ *                                  properties:
+ *                                      name:
+ *                                          type: string
+ *                                          description: nombre del producto
+ *                                          example: Bicicleta
+ *                                      images:
+ *                                          type: array
+ *                                          description: Arreglo de url de imagenes del producto
+ *                                          items:
+ *                                              type: string
+ *                                              description: url de imagen
+ *                                              example: https://firebasestorage.googleapis.com/v0/b/express-server-b8f0f.appspot.com/o/images%2Fbicicleta.jpg
+ *                                      brand:
+ *                                          type: string
+ *                                          description: Marca del producto
+ *                                          example: Corsario
+ *                                      price:
+ *                                          type: number
+ *                                          description: precio del producto
+ *                                          example: 250
+ *                                      description:
+ *                                          type: string
+ *                                          description: descripción del producto
+ *                                          example: Bicicleta de montaña
+ *                                      category:
+ *                                          type: string
+ *                                          description: categoria del producto
+ *                                          example: Bicicletas
+ *                                      condition:
+ *                                          type: string
+ *                                          description: condición del producto
+ *                                          example: Nuevo
+ *                                      seller:
+ *                                          type: string
+ *                                          description: uid del vendedor
+ *                                          example: 507f1f77bcf86cd799439011
+ *                                      location:
+ *                                          type: object
+ *                                          description: ubicación del producto
+ *                                          properties:
+ *                                              latitude:
+ *                                                  type: number
+ *                                                  description: latitud del producto
+ *                                                  example: -34.603722
+ *                                              longitude:
+ *                                                  type: number
+ *                                                  description: longitud del producto
+ *                                                  example: -58.381592
+ *          '500':
+ *              description: Error en el servidor
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                              error:
+ *                                  type: boolean
+ *                                  description: Indica que ocurrió un error
+ *                                  example: true
+ *                              message:
+ *                                  type: string
+ *                                  description: Mensaje indicando error
+ *                                  example: Ocurrió un error inesperado
+ *                              errorMessage:
+ *                                  type: string
+ *                                  description: Mensaje descriptivo sobre el error
+ *                                  example: Error en el servidor
+ */
+ router.get('/user/:uid', productController.getByUser);
 
 /**
  * @swagger
